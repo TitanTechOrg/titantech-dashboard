980ee55cea00395ef663b94a967be085fc5498f8
akira <chowakira@gmail.com>
2023-09-20 18:45:42 +0200

    reorganise app logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import Dashboard from './pages/dashboard';
+import { Toaster } from '@/components/ui/toaster';
+import { createBrowserRouter, RouteObject, RouterProvider } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { NextUIProvider } from '@nextui-org/react';
+import NoTokenPage from './pages/landing/index.tsx';
 
 function App() {
-    return <Dashboard />;
+    const queryClient = new QueryClient();
+    const routes: RouteObject[] = [
+        {
+            path: '/',
+            element: <NoTokenPage />,
+        },
+        {
+            path: '/:token',
+            loader: async ({ params }) => params?.token ?? null,
+            element: <Dashboard />,
+            errorElement: <NoTokenPage />,
+        },
+    ];
+    const router = createBrowserRouter(routes, { basename: import.meta.env.DEV ? '/' : '/titantech-dashboard/' });
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <NextUIProvider>
+                <RouterProvider router={router} />
+                <ReactQueryDevtools initialIsOpen={false} />
+                <Toaster />
+            </NextUIProvider>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ command }) => {
+    const config = {
+        plugins: [react()],
+        base: '/',
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
         },
-    },
+    };
+
+    if (command !== 'serve') {
+        config.base = '/titantech-dashboard/';
+    }
+
+    return config;
 });

abdce102f16f483d0eae538b8cf80a2c962b0db9
akira <chowakira@gmail.com>
2024-01-30 20:32:01 +0100

    add off-strat damage UI & slider

--- src/features/attacks/components/attacks-card.component.tsx
+++ src/features/attacks/components/attacks-card.component.tsx
@@ -1,9 +1,11 @@
-import { Button, Card, CardBody, CardHeader, Divider, Image, Spinner, Switch } from '@nextui-org/react';
+import { Button, Card, CardBody, CardHeader, Divider, Image, Slider, SliderValue, Spinner, Switch } from '@nextui-org/react';
 import { InfiniteData, UseInfiniteQueryResult } from '@tanstack/react-query';
 import { AttacksList, RaidLogs } from '..';
 import { useBoundStore } from '@/stores/bound.store';
 import { useMediaQueries } from '@react-hook/media-query';
 import AttacksCardLogo from '@/assets/Attack.webp';
+import { formatter } from '@/utils/number-formatter';
+import { usePreferencesStore } from '@/stores/preferences.store';
 
 export function AttacksCard({
     data,
@@ -23,6 +25,13 @@ export function AttacksCard({
     const hasData: boolean = !!(data && data?.pages && data?.pages[0]?.attack_logs?.length > 0);
 
     const { showRaidCards, setShowRaidCards } = useBoundStore();
+    const { offstratDamageThreshold, setOffstratDamageThreshold } = usePreferencesStore();
+
+    const handleChange = (value: SliderValue) => {
+        if (isNaN(Number(value))) return;
+
+        setOffstratDamageThreshold(value as number);
+    };
 
     return (
         <Card className="p-2 dark:bg-neutral-800">
@@ -43,11 +52,45 @@ export function AttacksCard({
                 {error && <div>'An error has occurred: ' + {error.message}</div>}
                 {hasData ? (
                     <>
-                        {matches.width && (
-                            <Switch isSelected={showRaidCards} onValueChange={setShowRaidCards}>
-                                View Raid Decks
-                            </Switch>
-                        )}
+                        <div className="flex w-full max-w-md flex-col gap-6">
+                            {matches.width && (
+                                <Switch isSelected={showRaidCards} onValueChange={setShowRaidCards}>
+                                    <span className="text-sm font-medium">View Raid Decks</span>
+                                </Switch>
+                            )}
+                            {matches.width ? (
+                                !showRaidCards ? (
+                                    <Slider
+                                        size="md"
+                                        className="max-w-md"
+                                        color="primary"
+                                        label={<span className="text-sm font-medium">Offstrat Damage Threshold</span>}
+                                        showSteps={true}
+                                        step={500_000}
+                                        maxValue={5_000_000}
+                                        minValue={0}
+                                        value={offstratDamageThreshold}
+                                        getValue={(value) => formatter().format(value as number)}
+                                        onChange={handleChange}
+                                    />
+                                ) : null
+                            ) : (
+                                <Slider
+                                    size="md"
+                                    className="max-w-md"
+                                    color="primary"
+                                    label={<span className="text-sm font-medium">Offstrat Damage Threshold</span>}
+                                    showSteps={true}
+                                    step={500_000}
+                                    maxValue={5_000_000}
+                                    minValue={0}
+                                    value={offstratDamageThreshold}
+                                    getValue={(value) => formatter().format(value as number)}
+                                    onChange={handleChange}
+                                />
+                            )}
+                        </div>
+
                         <AttacksList
                             pages={data?.pages!}
                             fetchNextPage={fetchNextPage}
--- src/features/attacks/components/quick-view.component.tsx
+++ src/features/attacks/components/quick-view.component.tsx
@@ -1,6 +1,10 @@
-import { TitanSequence } from '@/features/titans';
-import { MappedArmourColour, MappedArmourType, MappedBodyColour, MappedBodyType, TitanPart } from '..';
+import { Image } from '@nextui-org/react';
+import { TitanSequence, TitanSequenceParts } from '@/features/titans';
 import { percentage } from '@/utils/number-formatter';
+import { MappedArmourColour, MappedArmourType, MappedBodyColour, MappedBodyType, TitanPart } from '..';
+
+import OffStratDamage from '@/assets/titanPartTargets/off-strat-damage.webp';
+import { usePreferencesStore } from '@/stores/preferences.store';
 
 type QuickViewProps = {
     data: TitanPart[];
@@ -29,6 +33,37 @@ const getBackgroundKey = (findPartOnName: string, parts: TitanPart[], totalDamag
     return [foundPart, key];
 };
 
+const isDamagedOffstratPart = (
+    offstratDamageThreshold: number,
+    partArmourName: string,
+    partBodyName: string,
+    parts: TitanSequenceParts[] | undefined,
+    damagedParts: TitanPart[]
+) => {
+    if (!parts) return false;
+
+    const foundArmourPart = parts.find((part) => part.name === partArmourName);
+    const foundBodyPart = parts.find((part) => part.name === partBodyName);
+
+    const isOffstratPart = !foundArmourPart?.target || !foundBodyPart?.target;
+
+    if (!isOffstratPart) return false;
+
+    const foundDamagedArmourPart = damagedParts.find((part) => part.name === partArmourName);
+    const foundDamagedBodyPart = damagedParts.find((part) => part.name === partArmourName);
+
+    const hasDamagedOffstratPart = !!foundDamagedArmourPart || !!foundDamagedBodyPart;
+
+    if (!hasDamagedOffstratPart) return false;
+
+    const armourDamage = foundDamagedArmourPart?.value ?? 0;
+    const bodyDamage = foundDamagedBodyPart?.value ?? 0;
+
+    const hasOffstratDamageOverThreshold = armourDamage > offstratDamageThreshold || bodyDamage > offstratDamageThreshold;
+
+    return hasOffstratDamageOverThreshold;
+};
+
 const getGradientColour = (armourPart: PartAndColour, bodyPart: PartAndColour) => {
     const hasArmourPart = armourPart[0] != null;
     const hasBodyPart = bodyPart[0] != null;
@@ -47,83 +82,181 @@ const getGradientColour = (armourPart: PartAndColour, bodyPart: PartAndColour) =
 };
 
 export function QuickView({ data, titan, isCompactView = false }: QuickViewProps) {
+    const { offstratDamageThreshold } = usePreferencesStore();
+
     const totalDamage = data.reduce((prev, curr) => prev + curr.value, 0);
 
     const classes = isCompactView
         ? `flex flex-col text-center items-center justify-center scale-75 max-h-16`
-        : `flex flex-col text-center absolute -top-12 right-16`;
+        : `flex flex-col text-center absolute -top-14 right-16 scale-90`;
 
     return (
         <div className={`${classes}`}>
             <span>{titan?.name}</span>
             <div className="flex flex-row items-center justify-center gap-0">
                 <div
-                    className={`min-w-6 border-1 border-gray-400 ${getGradientColour(
+                    className={`relative h-6 w-6 border-1 border-gray-400 ${getGradientColour(
                         getBackgroundKey('ArmorArmUpperRight', data, totalDamage),
                         getBackgroundKey('BodyArmUpperRight', data, totalDamage)
                     )}`}
                 >
-                    &nbsp;
+                    {isDamagedOffstratPart(offstratDamageThreshold, 'ArmorArmUpperRight', 'BodyArmUpperRight', titan?.parts, data) ? (
+                        <div className="h-full min-h-6 w-full min-w-6">
+                            <Image
+                                width="100%"
+                                height="100%"
+                                className="absolute left-0 top-1/2 scale-[33%]"
+                                src={OffStratDamage}
+                                alt="Off-strat damage"
+                            />
+                        </div>
+                    ) : (
+                        <span>&nbsp;</span>
+                    )}
                 </div>
                 <div
-                    className={`min-w-6 border-1 border-x-0 border-gray-400 ${getGradientColour(
+                    className={`relative h-6 w-6 border-1 border-x-0 border-gray-400 ${getGradientColour(
                         getBackgroundKey('ArmorHead', data, totalDamage),
                         getBackgroundKey('BodyHead', data, totalDamage)
                     )}`}
                 >
-                    &nbsp;
+                    {isDamagedOffstratPart(offstratDamageThreshold, 'ArmorHead', 'BodyHead', titan?.parts, data) ? (
+                        <div className="h-full min-h-6 w-full min-w-6">
+                            <Image
+                                width="100%"
+                                height="100%"
+                                className="absolute left-0 top-1/2 scale-[33%]"
+                                src={OffStratDamage}
+                                alt="Off-strat damage"
+                            />
+                        </div>
+                    ) : (
+                        <span>&nbsp;</span>
+                    )}
                 </div>
                 <div
-                    className={`min-w-6 border-1 border-gray-400 ${getGradientColour(
+                    className={`relative h-6 w-6 border-1 border-gray-400 ${getGradientColour(
                         getBackgroundKey('ArmorArmUpperLeft', data, totalDamage),
                         getBackgroundKey('BodyArmUpperLeft', data, totalDamage)
                     )}`}
                 >
-                    &nbsp;
+                    {isDamagedOffstratPart(offstratDamageThreshold, 'ArmorArmUpperLeft', 'BodyArmUpperLeft', titan?.parts, data) ? (
+                        <div className="h-full min-h-6 w-full min-w-6">
+                            <Image
+                                width="100%"
+                                height="100%"
+                                className="absolute left-0 top-1/2 scale-[33%]"
+                                src={OffStratDamage}
+                                alt="Off-strat damage"
+                            />
+                        </div>
+                    ) : (
+                        <span>&nbsp;</span>
+                    )}
                 </div>
             </div>
             <div className="flex flex-row items-center justify-center gap-0">
                 <div
-                    className={`min-w-6 border-1 border-t-0 border-gray-400 ${getGradientColour(
+                    className={`relative h-6 w-6 border-1 border-t-0 border-gray-400 ${getGradientColour(
                         getBackgroundKey('ArmorHandRight', data, totalDamage),
                         getBackgroundKey('BodyHandRight', data, totalDamage)
                     )}`}
                 >
-                    &nbsp;
+                    {isDamagedOffstratPart(offstratDamageThreshold, 'ArmorHandRight', 'BodyHandRight', titan?.parts, data) ? (
+                        <div className="h-full min-h-6 w-full min-w-6">
+                            <Image
+                                width="100%"
+                                height="100%"
+                                className="absolute left-0 top-1/2 scale-[33%]"
+                                src={OffStratDamage}
+                                alt="Off-strat damage"
+                            />
+                        </div>
+                    ) : (
+                        <span>&nbsp;</span>
+                    )}
                 </div>
                 <div
-                    className={`min-w-6 border-1 border-x-0 border-t-0 border-gray-400 ${getGradientColour(
+                    className={`relative h-6 w-6 border-1 border-x-0 border-t-0 border-gray-400 ${getGradientColour(
                         getBackgroundKey('ArmorChestUpper', data, totalDamage),
                         getBackgroundKey('BodyChestUpper', data, totalDamage)
                     )}`}
                 >
-                    &nbsp;
+                    {isDamagedOffstratPart(offstratDamageThreshold, 'ArmorChestUpper', 'BodyChestUpper', titan?.parts, data) ? (
+                        <div className="h-full min-h-6 w-full min-w-6">
+                            <Image
+                                width="100%"
+                                height="100%"
+                                className="absolute left-0 top-1/2 scale-[33%]"
+                                src={OffStratDamage}
+                                alt="Off-strat damage"
+                            />
+                        </div>
+                    ) : (
+                        <span>&nbsp;</span>
+                    )}
                 </div>
                 <div
-                    className={`min-w-6 border-1 border-t-0 border-gray-400 ${getGradientColour(
+                    className={`relative h-6 w-6 border-1 border-t-0 border-gray-400 ${getGradientColour(
                         getBackgroundKey('ArmorHandLeft', data, totalDamage),
                         getBackgroundKey('BodyHandLeft', data, totalDamage)
                     )}`}
                 >
-                    &nbsp;
+                    {isDamagedOffstratPart(offstratDamageThreshold, 'ArmorHandLeft', 'BodyHandLeft', titan?.parts, data) ? (
+                        <div className="h-full min-h-6 w-full min-w-6 ">
+                            <Image
+                                width="100%"
+                                height="100%"
+                                className="absolute left-0 top-1/2 scale-[33%]"
+                                src={OffStratDamage}
+                                alt="Off-strat damage"
+                            />
+                        </div>
+                    ) : (
+                        <span>&nbsp;</span>
+                    )}
                 </div>
             </div>
             <div className="flex flex-row items-center justify-center gap-0">
                 <div
-                    className={`min-w-6 border-1 border-r-0 border-t-0 border-gray-400 ${getGradientColour(
+                    className={`relative h-6 w-6 border-1 border-r-0 border-t-0 border-gray-400 ${getGradientColour(
                         getBackgroundKey('ArmorLegUpperRight', data, totalDamage),
                         getBackgroundKey('BodyLegUpperRight', data, totalDamage)
                     )}`}
                 >
-                    &nbsp;
+                    {isDamagedOffstratPart(offstratDamageThreshold, 'ArmorLegUpperRight', 'BodyLegUpperRight', titan?.parts, data) ? (
+                        <div className="h-full min-h-6 w-full min-w-6">
+                            <Image
+                                width="100%"
+                                height="100%"
+                                className="absolute left-0 top-1/2 scale-[33%]"
+                                src={OffStratDamage}
+                                alt="Off-strat damage"
+                            />
+                        </div>
+                    ) : (
+                        <span>&nbsp;</span>
+                    )}
                 </div>
                 <div
-                    className={`min-w-6 border-1 border-t-0 border-gray-400 ${getGradientColour(
+                    className={`relative h-6 w-6 border-1 border-t-0 border-gray-400 ${getGradientColour(
                         getBackgroundKey('ArmorLegUpperLeft', data, totalDamage),
                         getBackgroundKey('BodyLegUpperLeft', data, totalDamage)
                     )}`}
                 >
-                    &nbsp;
+                    {isDamagedOffstratPart(offstratDamageThreshold, 'ArmorLegUpperLeft', 'BodyLegUpperLeft', titan?.parts, data) ? (
+                        <div className="h-full min-h-6 w-full min-w-6 sm:min-h-5 sm:min-w-5">
+                            <Image
+                                width="100%"
+                                height="100%"
+                                className="absolute left-0 top-1/2 scale-[33%]"
+                                src={OffStratDamage}
+                                alt="Off-strat damage"
+                            />
+                        </div>
+                    ) : (
+                        <span>&nbsp;</span>
+                    )}
                 </div>
             </div>
         </div>

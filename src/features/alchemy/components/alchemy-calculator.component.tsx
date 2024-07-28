53ab1e0f6b6f6cee36abff4b19d212302c9506ee
akira <chowakira@gmail.com>
2024-07-28 15:45:20 +0200

    update ingredients list for alchemy event

--- src/features/alchemy/components/alchemy-calculator.component.tsx
+++ src/features/alchemy/components/alchemy-calculator.component.tsx
@@ -1,31 +1,30 @@
 import { Controller, FieldValues, useForm } from 'react-hook-form';
 // import { zodResolver } from '@hookform/resolvers/zod';
-import * as z from 'zod';
-import { Button, Input, Code, Image, Switch } from '@nextui-org/react';
+import { Button, Code, Image, Input, Switch } from '@nextui-org/react';
 import { useCallback } from 'react';
+import * as z from 'zod';
 import { useCalculateAlchemy } from '..';
-
 // import Acorn from '@/assets/alchemy/Acorn.webp';
 // import Petal from '@/assets/alchemy/Petal.webp';
 // import Berries from '@/assets/alchemy/Berries.webp';
-// import Pepper from '@/assets/alchemy/Pepper.webp';
-
-import Leaf from '@/assets/alchemy/Leaf.webp';
-import Sand from '@/assets/alchemy/Sand.webp';
-import Steel from '@/assets/alchemy/Steel.webp';
-import Lightning from '@/assets/alchemy/Lightning.webp';
-import Scale from '@/assets/alchemy/Scale.webp';
-import Power from '@/assets/alchemy/Power.webp';
-import Flame from '@/assets/alchemy/Flame.webp';
+import Beetle from '@/assets/alchemy/Beetle.webp';
 import Crystal from '@/assets/alchemy/Crystal.webp';
 import Egg from '@/assets/alchemy/Egg.webp';
 import Essence from '@/assets/alchemy/Essence.webp';
 import Feather from '@/assets/alchemy/Feather.webp';
-import Spirit from '@/assets/alchemy/Spirit.webp';
-import Poison from '@/assets/alchemy/Poison.webp';
-import Tooth from '@/assets/alchemy/Tooth.webp';
-import Water from '@/assets/alchemy/Water.webp';
+import Flame from '@/assets/alchemy/Flame.webp';
+import Leaf from '@/assets/alchemy/Leaf.webp';
+import Lightning from '@/assets/alchemy/Lightning.webp';
+import Pepper from '@/assets/alchemy/Pepper.webp';
+// import Poison from '@/assets/alchemy/Poison.webp';
+import Power from '@/assets/alchemy/Power.webp';
+import Sand from '@/assets/alchemy/Sand.webp';
+import Scale from '@/assets/alchemy/Scale.webp';
 import Shadow from '@/assets/alchemy/Shadow.webp';
+import Spirit from '@/assets/alchemy/Spirit.webp';
+import Steel from '@/assets/alchemy/Steel.webp';
+import Tooth from '@/assets/alchemy/Tooth.webp';
+// import Water from '@/assets/alchemy/Water.webp';
 
 const validation = z
     .number()
@@ -33,28 +32,32 @@ const validation = z
     .min(0, { message: 'Required' })
     .max(128, { message: 'Must be between 0 and 128' });
 
-const ingredients = [
+const ingredients: {
+    name: string;
+    fieldType: z.ZodNumber;
+    imgUrl: string;
+}[] = [
     // { name: 'Acorn', fieldType: validation, imgUrl: Acorn },
     // { name: 'Petal', fieldType: validation, imgUrl: Petal },
     // { name: 'Berries', fieldType: validation, imgUrl: Berries },
-    // { name: 'Pepper', fieldType: validation, imgUrl: Pepper },
-
-    { name: 'Steel', fieldType: validation, imgUrl: Steel },
-    { name: 'Sand', fieldType: validation, imgUrl: Sand },
-    { name: 'Water', fieldType: validation, imgUrl: Water },
-    { name: 'Flame', fieldType: validation, imgUrl: Flame },
-    { name: 'Lightning', fieldType: validation, imgUrl: Lightning },
+    // { name: 'Water', fieldType: validation, imgUrl: Water },
+    // { name: 'Poison', fieldType: validation, imgUrl: Poison },
     { name: 'Leaf', fieldType: validation, imgUrl: Leaf },
-    { name: 'Scale', fieldType: validation, imgUrl: Scale },
-    { name: 'Egg', fieldType: validation, imgUrl: Egg },
-    { name: 'Crystal', fieldType: validation, imgUrl: Crystal },
-    { name: 'Poison', fieldType: validation, imgUrl: Poison },
-    { name: 'Power', fieldType: validation, imgUrl: Power },
-    { name: 'Tooth', fieldType: validation, imgUrl: Tooth },
+    { name: 'Sand', fieldType: validation, imgUrl: Sand },
+    { name: 'Flame', fieldType: validation, imgUrl: Flame },
+    { name: 'Pepper', fieldType: validation, imgUrl: Pepper },
+    { name: 'Steel', fieldType: validation, imgUrl: Steel },
     { name: 'Feather', fieldType: validation, imgUrl: Feather },
+    { name: 'Beetle', fieldType: validation, imgUrl: Beetle },
+    { name: 'Lightning', fieldType: validation, imgUrl: Lightning },
+    { name: 'Scale', fieldType: validation, imgUrl: Scale },
+    { name: 'Crystal', fieldType: validation, imgUrl: Crystal },
+    { name: 'Tooth', fieldType: validation, imgUrl: Tooth },
+    { name: 'Egg', fieldType: validation, imgUrl: Egg },
     { name: 'Shadow', fieldType: validation, imgUrl: Shadow },
-    { name: 'Essence', fieldType: validation, imgUrl: Essence },
     { name: 'Spirit', fieldType: validation, imgUrl: Spirit },
+    { name: 'Essence', fieldType: validation, imgUrl: Essence },
+    { name: 'Power', fieldType: validation, imgUrl: Power },
 ];
 
 type Ingredients = (typeof ingredients)[0];
